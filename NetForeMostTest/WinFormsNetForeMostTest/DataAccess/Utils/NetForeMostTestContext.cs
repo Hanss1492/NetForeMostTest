b2fa8b17fad3850de69ff9582a4d9fd3c9ca7e34
Hanss <hanssel_navarro@hotmail.com>
2024-03-07 00:37:32 -0600

    Context

---
+++ NetForeMostTest/WinFormsNetForeMostTest/DataAccess/Utils/NetForeMostTestContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WinFormsNetForeMostTest.DataAccess.Models;
+
+namespace WinFormsNetForeMostTest.DataAcess.Utils;
+
+public class NetForeMostTestContext : DbContext
+{
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+
+    public NetForeMostTestContext(DbContextOptions<NetForeMostTestContext> options)
+        : base(options)
+    {
+
+    }
+}
